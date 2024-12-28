7ef8630913abc9710375a15bf1cdcb0fb4c0f9ba
Abdur Rahman <ar.abdurrahmaneee@gmail.com>
2024-12-28 20:39:46 +0100

    committing before uplaoding to python everywhere server

--- server/server.py
+++ server/server.py
@@ -1,26 +1,46 @@
-from flask import Flask, render_template, send_from_directory, url_for
+from flask import Flask, render_template, send_from_directory, url_for, request, redirect
 import os
+import csv
+
+from jinja2.lexer import newline_re
+
 app = Flask(__name__)
 print(__name__)
 
 
-@app.route("/<username>/<int:post_id>")
-def hello_world(username=None, post_id=None):
-    return render_template('index.html', name=username, post_id=post_id)
+@app.route("/")
+def my_home():
+    return render_template('index.html')
 
-@app.route("/about.html")
-def about():
-    return render_template('about.html')
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
 
-@app.route("/blog")
-def blog():
-    return 'these are my blogs'
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            print(data)
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'did not save to database'
+    else:
+        return 'something went wrong, Try again'
 
-@app.route("/blog/2020/dogs")
-def blog2():
-    return 'this is not my dog'
+@app.route('/submit_form', methods=['POST', 'GET'])
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'\n{email},{subject},{message}')
 
-# @app.route("/favicon.ico")
-# def favicon():
-#     return send_from_directory(os.path.join(app.root_path, 'static'),
-#                                'static/pic.ico')
+def write_to_csv(data):
+    with open('database.csv', mode='a',newline='') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database, delimiter=',', quotechar='"',quoting= csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
